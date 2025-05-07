84a817c3f7a9c64151a4d3fa76c062bc2dae2d05
CaetSiq05 <caetano.oliva@unesp.br>
2025-05-07 15:33:56 -0300

    Contagem
    Adicionei uma contagem de 0 a k a partir de um laço for

--- Nomedeumarquivo.cpp
+++ Nomedeumarquivo.cpp
@@ -5,5 +5,10 @@ int main(){
 
     cout << "Digite um numero inteiro de 1 a 10:\n";
     cin >> k;
-    cout << "O numero que voce digitou: "<< k;
+    cout << "O numero que voce digitou: "<< k << endl;
+
+    cout << "Contagem:\n";
+    for(int i = 0; i <= k; i++){
+        cout << i << endl;
+    }
 }
