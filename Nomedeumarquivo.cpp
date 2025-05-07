c0f743cc751c387650f768b21a05eac15f0d7191
CaetSiq05 <caetano.oliva@unesp.br>
2025-05-07 15:21:38 -0300

    Criei um arquivo de teste
    Criei um arquivo de teste para ler uma variável n

---
+++ Nomedeumarquivo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+
+    cout << "Digite um numero inteiro de 1 a 10:\n";
+    cin >> n;
+    cout << "O numero que voce digitou: "<< n;
+}
